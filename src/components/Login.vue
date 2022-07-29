89a40b897f294a002109f3a2d804407f545eae68
leo-rp <leorock1988@hotmail.com>
2022-07-29 18:49:41 -0500

    added Login.vue component

---
+++ src/components/Login.vue
@@ -0,0 +1,42 @@
+<template>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center" dense>
+          <v-col cols="12" sm="8" md="4" lg="4">         
+            <v-card elevation="0">
+              <div class="text-center">
+                <h1 class="mb-2">Login</h1>
+              </div>
+              <a href="https://github.com/vuetifyjs/vuetify/releases/tag/v2.6.8" name="https://github.com/vuetifyjs/vuetify/releases/tag/v2.6.8" title="https://github.com/vuetifyjs/vuetify/releases/tag/v2.6.8" target="_blank">
+                <v-img src="/src/assets/logo.svg" alt="vuetify" contain height="200"></v-img>
+              </a>
+              <v-card-text>
+                <v-form>
+                  <v-text-field label="Enter your email" name="email" prepend-inner-icon="mdi-email" type="email" class="rounded-0" outlined >
+                  </v-text-field>
+                  <v-text-field label="Enter your password" name="password" prepend-inner-icon="mdi-lock" type="password" suffix="| Forgot?" class="rounded-0" outlined></v-text-field>
+                  <v-btn class="rounded-0" color="#000000" x-large block dark>Login</v-btn>
+                  <v-card-actions class="text--secondary">
+                    <v-checkbox color="#000000" label="Remember me"></v-checkbox>
+                    <v-spacer></v-spacer>
+                    
+                    No account? <a href="#" class="pl-2" style="color: #000000">Sign Up</a>
+                  </v-card-actions>
+                </v-form>
+              </v-card-text>
+              <v-card-actions class="ml-6 mr-6 text-center">
+                <p>Powred By WSS</p>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+</template>
+
+<script>
+export default {
+  name: 'app-login'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
